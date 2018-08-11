f85e65992b6957c3ee74d90ac754971c644bf1c3
Simon <simonirang92@hotmail.com>
2018-08-10 21:14:16 -0400

    Config Update

--- aws_notification.py
+++ aws_notification.py
@@ -3,7 +3,7 @@ import boto3
 
 client = boto3.client('sns')
 
-def send_notification(AWS_Topic_ARN, Subject, Message):
+def aws_send_notification(AWS_Topic_ARN, Subject, Message):
     print ("Notification Sent")
     response = client.publish(TopicArn=AWS_Topic_ARN,
                               Subject=Subject,
--- sensor.py
+++ sensor.py
@@ -1,7 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 import configparser
-from aws_notification import send_notification
+from aws_notification import aws_send_notification
+
+
+
+
+
 
 def main():
     print ("Reading configuration file")
@@ -9,10 +14,12 @@ def main():
     config.read('config', encoding='utf-8')
     Start_Counter = int(config['General']['Start_Counter'])
     GPIO_Sensor_Pin = int(config['Sensor']['GPIO_Sensor_Pin'])
-    AWS_Topic_ARN = config['Notification']['AWS_Topic_ARN']
-    Subject = config['Notification']['Subject']
-    Message = config['Notification']['Message']
+    AWS_Notification = config['AWS_Notification']['AWS_Notification']
+    AWS_Topic_ARN = config['AWS_Notification']['AWS_Topic_ARN']
+    Subject = config['Notification']['Motion_Subject']
+    Message = config['Notification']['Motion_Message']
     Timeout = int(config['Notification']['Notification_Timeout'])
+    Limit = int(config['Notification']['Notification_Limit'])
 
     print ("Waiting for sensor to start")
     time.sleep(Start_Counter)
@@ -28,9 +35,9 @@ def main():
         motion = GPIO.input(GPIO_Sensor_Pin)
         if motion == 1:
             print ("Motion Detected!")
-            if c < 1:
+            if c < Limit:
                 print ("Sending Notification")
-                #send_notification(AWS_Topic_ARN, Subject, Message)
+                aws_send_notification(AWS_Topic_ARN, Subject, Message)
                 c = c + 1
             time.sleep(Timeout)
 
